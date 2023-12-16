8f4c3ea04b0156e191a14dfde8a0e75c18284089
KsiRT <130671776+KsiRT@users.noreply.github.com>
2023-12-16 18:40:51 +0200

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -30,6 +30,6 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  background-color: #8295b8;
+  background-color: #e5d1b8;
   height: 100vh;
 `;
--- src/components/Statistics/StatisticsStyled.jsx
+++ src/components/Statistics/StatisticsStyled.jsx
@@ -12,6 +12,7 @@ export const List = styled.ul`
 export const Item = styled.li`
   padding: 10px;
   font-size: 20px;
-  border: 1px solid #ccc;
-  border-radius: 8px;
+  border: 1px solid #6a6f4c;
+  border-radius: 12px;
+  background-color: #cbb89d;
 `;
